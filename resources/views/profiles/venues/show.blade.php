af6514196a4f8733ee880dc2213218ada32374b6
Privat <privat@florians-mbp.home>
2023-10-16 22:55:44 +0200

    bug V10 fixed

--- resources/views/profiles/venues/show.blade.php
+++ resources/views/profiles/venues/show.blade.php
@@ -4,7 +4,7 @@
     <h1 class="d-inline-block">{{ $venue->name }}</h1><h3 class="d-inline-block text-secondary">#{{ $venue->tag }}</h3>
     @foreach($users as $user)
         @if($user->venues()->exists() && $user->id === Auth::id())
-            <a href="/venues/{{ $venue->id }}/edit"><button class="d-inline-block btn btn-primary float-right">Edit</button></a>
+            <a href="/venues/{{ $venue->id }}/edit"><button class="d-inline-block btn btn-primary float-end">Edit</button></a>
         @endif
     @endforeach
     @if($venue->description != null)

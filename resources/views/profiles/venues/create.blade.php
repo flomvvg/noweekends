32c361399ed458bed75b2624bca25e46554d661c
Privat <privat@florians-mbp.home>
2023-10-16 22:52:46 +0200

    bug V1 fixed

--- resources/views/profiles/venues/create.blade.php
+++ resources/views/profiles/venues/create.blade.php
@@ -1,17 +1,6 @@
 @include('base.base')
 @include('base.nav')
 <div class="container">
-    @if($errors->any())
-        @foreach($errors->all() as $error)
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{ $error }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        @endforeach
-    @endif
-
     <h1>Create Venue Profile</h1>
     <form action="/venues" method="POST">
         @csrf

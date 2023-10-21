48f9c81bb47936889bc8a98960497238fecf7d09
Privat <privat@florians-mbp.home>
2023-10-21 17:37:44 +0200

    finis v2

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('events.index');
 });
 
 Route::resource('users', UserController::class);
